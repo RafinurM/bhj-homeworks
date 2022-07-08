9cef6f82e3a5cdd20dfbb0ea4ef9106a86a9ee58
RafinurM <zadorinka16@mail.ru>
2022-07-09 06:20:18 +0700

    Resolve task

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,26 @@
+let deadCount = document.getElementById("dead");
+let missCount = document.getElementById("lost");
+let mole = document.getElementsByClassName(".hole_has-mole");
+
+for (let i = 1; i < 9; i++) {
+  let hole = document.getElementById(`hole${i}`);
+  hole.onclick = function () {
+    if (hole.className.includes("hole_has-mole")) {
+      deadCount.textContent++;
+    } else [missCount.textContent++];
+  };
+}
+
+let winLoss = function() {
+    if (deadCount.textContent === '10') {
+        alert('Победа!');
+        deadCount.textContent = 0;
+        missCount.textContent = 0;
+    } else if (missCount.textContent === '10') {
+        alert('Поражение!');
+        deadCount.textContent = 0;
+        missCount.textContent = 0;
+    }
+}
+
+setInterval(winLoss, 100)
